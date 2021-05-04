be45dc190f3afdbdcbdb48d3e9d47d6e414859b4
Tomas D'Stefano <tomas_stefano@successoft.com>
2021-05-04 15:42:15 -0300

    Add london as timezone

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,8 @@ module FbMetadataApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.time_zone = 'London'
   end
 end
 
