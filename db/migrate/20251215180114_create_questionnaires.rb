9916e1f5183fa5d9c827ec0744794ce77de48009
Ripan Kumar <er.ripankumar@gmail.com>
2025-12-12 16:38:29 +0000

    Create db schema and validate questionnaire submission, Update Makefile

---
+++ db/migrate/20251215180114_create_questionnaires.rb
@@ -0,0 +1,17 @@
+class CreateQuestionnaires < ActiveRecord::Migration[7.2]
+  def change
+    create_table :questionnaires do |t|
+      t.string :new_form_reason, null: false
+      t.boolean :govuk_forms_ruled_out
+      t.jsonb :required_moj_forms_features
+      t.text :govuk_forms_ruled_out_reason
+      t.boolean :continue_with_moj_forms
+      t.string :estimated_page_count
+      t.string :estimated_first_year_submissions_count
+      t.string :submission_delivery_method
+      t.references :service, type: :uuid, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
